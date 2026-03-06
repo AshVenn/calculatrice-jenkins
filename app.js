19c3b6309d2aa29a9463430f454adebb0694f48e
Youness Zouhairi <sora7sk@gmail.com>
2026-03-06 18:13:34 +0000

    Test webhook Jenkins3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API calculatrice opérationnelle - version 2");
+  res.send("API calculatrice opérationnelle - version 3");
 });
 
 app.get("/add", (req, res) => {
